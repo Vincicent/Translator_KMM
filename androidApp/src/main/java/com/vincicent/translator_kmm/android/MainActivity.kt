37f8ed08fd88c745bfa5b047c97fc39332ca75c7
Vincicent <Prive@.com>
2024-06-06 15:57:55 +0200

    Add floating button and update navigation

--- androidApp/src/main/java/com/vincicent/translator_kmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/vincicent/translator_kmm/android/MainActivity.kt
@@ -10,12 +10,15 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.vincicent.translator_kmm.android.core.presentation.navigation.Routes
 import com.vincicent.translator_kmm.android.translate.presentation.AndroidTranslateViewModel
 import com.vincicent.translator_kmm.android.translate.presentation.TranslateScreen
+import com.vincicent.translator_kmm.translate.presentation.TranslateEvent
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -47,8 +50,26 @@ fun TranslatorRoot() {
             val state by viewModel.state.collectAsState()
             TranslateScreen(
                 state = state,
-                onEvent = viewModel::onEvent
+                onEvent = { event ->
+                    when (event) {
+                        is TranslateEvent.RecordAudio -> {
+                            navController.navigate(Routes.VOICE_TO_TEXT + "/${state.fromLanguage.language.langCode}")
+                        }
+                        else -> viewModel.onEvent(event)
+                    }
+                }
             )
+        }
+        composable(
+            route = Routes.VOICE_TO_TEXT + "/{languageCode}",
+            arguments = listOf(
+                navArgument("languageCode") {
+                    type = NavType.StringType
+                    defaultValue = "en"
+                }
+            )
+        ) {
+            Text(text = "Voice-to-text")
         }
     }
 }
--- androidApp/src/main/java/com/vincicent/translator_kmm/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/com/vincicent/translator_kmm/android/translate/presentation/TranslateScreen.kt
@@ -2,6 +2,7 @@ package com.vincicent.translator_kmm.android.translate.presentation
 
 import android.speech.tts.TextToSpeech
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,9 +10,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -19,11 +26,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
 import com.vincicent.translator_kmm.android.R
@@ -60,8 +70,23 @@ fun TranslateScreen(
 
     Scaffold(
         floatingActionButton = {
-
-        }
+            FloatingActionButton(
+                onClick = {
+                    onEvent(TranslateEvent.RecordAudio)
+                },
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary,
+                modifier = Modifier
+                    .size(75.dp)
+                    .clip(CircleShape)
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.mic),
+                    contentDescription = stringResource(id = R.string.record_audio)
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center
     ) { padding ->
         LazyColumn(
             modifier = Modifier
