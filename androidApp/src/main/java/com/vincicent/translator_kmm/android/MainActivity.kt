f0161886ec2ddc45dd374045c7574e3db3a26ded
Vincicent <Prive@.com>
2024-06-06 14:11:43 +0200

    setup navigation

--- androidApp/src/main/java/com/vincicent/translator_kmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/vincicent/translator_kmm/android/MainActivity.kt
@@ -6,10 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.vincicent.translator_kmm.Greeting
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.vincicent.translator_kmm.android.core.presentation.navigation.Routes
+import com.vincicent.translator_kmm.android.translate.presentation.AndroidTranslateViewModel
+import com.vincicent.translator_kmm.android.translate.presentation.TranslateScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    TranslatorRoot()
                 }
             }
         }
@@ -27,14 +36,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    TranslatorTheme {
-        GreetingView("Hello, Android!")
+fun TranslatorRoot() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Routes.TRANSLATE
+    ) {
+        composable(route = Routes.TRANSLATE) {
+            val viewModel = hiltViewModel<AndroidTranslateViewModel>()
+            val state by viewModel.state.collectAsState()
+            TranslateScreen(
+                state = state,
+                onEvent = viewModel::onEvent
+            )
+        }
     }
 }
---
+++ androidApp/src/main/java/com/vincicent/translator_kmm/android/core/presentation/navigation/Routes.kt
@@ -0,0 +1,6 @@
+package com.vincicent.translator_kmm.android.core.presentation.navigation
+
+object Routes {
+    const val TRANSLATE = "translate"
+    const val VOICE_TO_TEXT = "voice_to_text"
+}
