4b4989f89a2a04c7fea723a81121d100a45f9d92
Vincicent <Prive@.com>
2024-06-09 19:02:38 +0200

    (Android) Speech Recognition on Android

---
+++ shared/src/commonMain/kotlin/com/vincicent/voice_to_text/domain/VoiceToTextParser.kt
@@ -0,0 +1,11 @@
+package com.vincicent.voice_to_text.domain
+
+import com.vincicent.translator_kmm.core.domain.util.CommonStateFlow
+
+interface VoiceToTextParser {
+    val state: CommonStateFlow<VoiceToTextParserState>
+    fun startListening(languageCode: String)
+    fun stopListening()
+    fun cancel()
+    fun reset()
+}
